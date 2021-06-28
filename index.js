b104aeb33d8d1831c94b9722cf64a935917c7ad1
Melissa Armand <melissarmand@gmail.com>
2021-06-28 18:50:42 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -2,17 +2,53 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions=() => {
+    return inquirer.prompt ([ {
+        type: 'input',
+        name: 'titleofProject',
+        message: 'What is the title of your project?',
+     },
+     {
+         type: 'input',
+         name: 'description',
+         message: 'Give a brief description of your project.',
+     },
 
-];
+     {
+         type: 'input',
+         name: 'installation',
+         message: 'What are the steps required to install your project?',
+     },
+     {
+         type: 'input',
+         name: 'credits',
+         message: 'List your collaborators, if any, and any tutorials you may have used.',
+     },
+     {  type: 'input',
+        name: 'license',
+        message: 'Please add any licenses used for this project'
+     },
+        
+    ])
+};
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile('generateMarkdown.js', generateMarkdown) {
+    fs.writeFile('generateMarkdown', generateMarkdown)
+}
 
 // TODO: Create a function to initialize app
 function init() {}
 
+// const init = () => {
+    // promptUser()
+    //   .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+    //   .then(() => console.log('Successfully wrote to index.html'))
+    //   .catch((err) => console.error(err));
+//   };
 // Function call to initialize app
 init();
