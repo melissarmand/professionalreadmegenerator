75d30f31ef7ae177dcc383108eaf0c6c403431ea
Melissa Armand <melissarmand@gmail.com>
2021-07-01 06:36:46 -0400

    switch&case statements

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  
-}
+ function renderLicenseBadge(license) {
+  switch (answers.license) {
+    case 'Apache':
+      licenseBadge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+      break;
+    
+    case 'IBM':
+      licenseBadge = `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`;
+      break;
+
+      case 'MIT':
+        licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+        break;
+
+      case 'Mozilla':
+        licenseBadge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+        break;
+      
+      case 'ODbL':
+        licenseBadge = `[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)`;
+        break;
+
+      case 'WTFPL':
+        licenseBadge = `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`;
+        break;
+
+      default: '';
+        break;
+  }}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,7 +39,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  
+
 }
 
 // TODO: Create a function to generate markdown for README
@@ -31,8 +57,9 @@ function generateMarkdown(answers) {
 
   ## License
   ${answers.license}
-
 `;
 }
+
+renderLicenseBadge (license);
 
 module.exports = generateMarkdown;
